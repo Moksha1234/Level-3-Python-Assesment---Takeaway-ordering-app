b831c8127e98a1652ab0ce3c16f2afcdf9574252
Moksha <128763560+Moksha1234@users.noreply.github.com>
2025-08-15 10:43:36 +1200

    this is just a test file
    testing to see how to upload

---
+++ Calculating area of a circle.py
@@ -0,0 +1,15 @@
+#Create a function to calculate area of a circle
+import math
+def area(radius): 
+    pi = math.pi
+    area = pi * radius**2
+    print(area)
+
+#ask inputs
+radius = float(input("enter radius: "))
+
+area(radius)
+    
+
+
+ 
